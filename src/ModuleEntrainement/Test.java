5f8cdcb43c1d3e858dc15475005a8a4d8dad75fa
Mariakarara <claind15@yahoo.fr>
2023-03-01 16:49:21 +0100

    add first file for Salameche pokemon

---
+++ src/ModuleEntrainement/Test.java
@@ -0,0 +1,15 @@
+package ModuleEntrainement;
+
+import java.util.List;
+import java.util.ArrayList;
+import ModuleReferentiel.Salameche;
+
+public static void allPokemon(){
+
+        // Déclaration d'une liste de Integer
+        ArrayList<Salameche> maPokeListTest = new ArrayList<>();
+
+        Salemeche crazySalameche = new Salemeche(name:"theCrazySalemeche", age:4);
+
+
+    }
